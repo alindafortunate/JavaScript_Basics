2f3a8babe44c2f2f1b21b5d454712b6b9041fea8
alindafortunate <alindafortunate5@gmail.com>
2025-07-09 22:10:37 +0300

    Understood the conditional statement of if else

--- conditional_statements.js
+++ conditional_statements.js
@@ -2,7 +2,16 @@
 
 if (5 === 4 && 4 === 5) {
     console.log("Hello world.")
-}
-else {
+} else {
     console.log('I am learning conditional statements')
+}
+
+let number = 0
+
+if (number % 2 === 0) {
+    console.log('This is an even number.')
+} else if (number % 2 !== 0) {
+    console.log('This is an odd number.')
+} else {
+    "The number is a complex one."
 }
