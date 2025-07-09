bf70b6cde4e0b4ee2af4b8832e9e8be2090f2f66
alindafortunate <alindafortunate5@gmail.com>
2025-07-09 21:33:29 +0300

    Understood the object data type.

---
+++ data_types.js
@@ -0,0 +1,22 @@
+let car = 'Benz'
+let number = 3
+let is_admin = false
+let defined_data
+let db_data = null
+
+let cars = {
+    'benz': 'blue',
+    'toyota': 'red',
+    'nissan': 'green'
+
+}
+let friuts = ['oranges', 'mangoes', 'ovacado', 'pawpaw']
+
+// console.log(typeof car)
+// console.log(typeof number)
+// console.log(typeof is_admin)
+// console.log(typeof defined_data)
+// console.log(typeof db_data)
+console.log(cars.benz)
+console.log(typeof cars)
+console.log(typeof friuts)
